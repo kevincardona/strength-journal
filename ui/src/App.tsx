1e2dd9d48ea7ee0ec7891a41dc369df2469c2267
Kevin Cardona <kevincardona1@gmail.com>
2022-11-24 02:26:20 -0700

    Add storybook, add react router

---
+++ ui/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" />
+        <Route path="/home" />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
